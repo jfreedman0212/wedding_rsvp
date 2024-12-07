bb7d69d0183ae8ff9f7c2c75ad50d57f11e84abd
Josh Freedman <josh@freedman.dev>
2024-12-06 22:09:12 -0500

    Add initial models for guests

--- guests/admin.py
+++ guests/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GuestGroup, Guest
+
+
+class GuestInline(admin.StackedInline):
+    model = Guest
+    extra = 3
+    min_num = 1
+    fields = ['name']
+
+
+class GuestGroupAdmin(admin.ModelAdmin):
+    fields = ['email']
+    inlines = [GuestInline]
+
+
+admin.site.register(Guest)
+admin.site.register(GuestGroup, GuestGroupAdmin)
--- guests/models.py
+++ guests/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+
+class GuestGroup(models.Model):
+    class GuestGroupStatusOptions(models.TextChoices):
+        RESPONDED = "RE", _("Responded")
+        NO_RESPONSE = "NR", _("No Response")
+
+    status = models.CharField(
+        max_length=2,
+        choices=GuestGroupStatusOptions,
+        default=GuestGroupStatusOptions.NO_RESPONSE
+    )
+    slug = ShortUUIDField(
+        length=10,
+        max_length=10,
+        alphabet='abcdefghjkmnprstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789',
+        primary_key=False,
+        unique=True
+    )
+    email = models.EmailField(unique=True)
+
+
+class Guest(models.Model):
+    class GuestStatusOptions(models.TextChoices):
+        GOING = "GO", _("Going")
+        NOT_GOING = "NG", _("Not Going")
+        NO_RESPONSE = "NR", _("No Response")
+
+    name = models.CharField(blank=False, null=False, max_length=300)
+    status = models.CharField(
+        max_length=2,
+        choices=GuestStatusOptions,
+        default=GuestStatusOptions.NO_RESPONSE
+    )
+    dietary_restrictions = models.TextField()
+    group = models.ForeignKey(
+        GuestGroup,
+        related_name="guests",
+        on_delete=models.CASCADE
+    )
