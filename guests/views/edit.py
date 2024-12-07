1c084655e1cb5216e183832e57872812465668fe
Josh Freedman <josh@freedman.dev>
2024-12-07 01:46:30 -0500

    Split views up into their own files

--- guests/views/edit.py
+++ guests/views/edit.py
@@ -1,42 +1,12 @@
 from django.urls import reverse_lazy
-from .models import Guest
+from guests.models import Guest
 from django.forms import modelformset_factory
 from django import forms
 from django.views.generic.edit import FormView
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 
-from . import models
-
-
-class RsvpRequestForm(forms.Form):
-    email = forms.EmailField(required=True)
-
-    def send_email(self):
-        try:
-            group = models.GuestGroup.objects.get(
-                email=self.cleaned_data['email']
-            )
-            models.GuestGroupEmailInvitation.objects.filter(
-                group=group
-            ).delete()
-            group.invitations.create(
-                expires_at=timezone.now() + timezone.timedelta(hours=2))
-            group.save()
-            # TODO: actually send the email!
-        except models.GuestGroup.DoesNotExist:
-            # just treat it as successful if no group exists
-            pass
-
-
-class RsvpRequestView(FormView):
-    template_name = "guests/index.html"
-    form_class = RsvpRequestForm
-    success_url = "/"
-
-    def form_valid(self, form):
-        form.send_email()
-        return super().form_valid(form)
+from guests import models
 
 
 class GuestForm(forms.ModelForm):
---
+++ guests/views/index.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.views.generic.edit import FormView
+from django.utils import timezone
+
+from guests import models
+
+
+class RsvpRequestForm(forms.Form):
+    email = forms.EmailField(required=True)
+
+    def send_email(self):
+        try:
+            group = models.GuestGroup.objects.get(
+                email=self.cleaned_data['email']
+            )
+            models.GuestGroupEmailInvitation.objects.filter(
+                group=group
+            ).delete()
+            group.invitations.create(
+                expires_at=timezone.now() + timezone.timedelta(hours=2))
+            group.save()
+            # TODO: actually send the email!
+        except models.GuestGroup.DoesNotExist:
+            # just treat it as successful if no group exists
+            pass
+
+
+class RsvpRequestView(FormView):
+    template_name = "guests/index.html"
+    form_class = RsvpRequestForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_email()
+        return super().form_valid(form)
